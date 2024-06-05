1c121c89874500004660c481eeb5b36f620bfe36
Volkov Slava <volkov.frontend@gmail.com>
2024-06-05 15:51:24 +0300

    feat: add sort items

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,6 +3,8 @@ import React, { useState } from 'react';
 import { Container, Typography, IconButton, AppBar, Toolbar, Tooltip } from '@mui/material';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
+import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
 
 import { useThemeContext } from './ThemeProvider/lib/useThemeContext';
 
@@ -20,6 +22,7 @@ const App: React.FC = () => {
   const { toggleTheme, isDarkMode } = useThemeContext();
   const [todos, setTodos] = useState<Todo[]>([]);
   const [filter, setFilter] = useState<'all' | 'active' | 'completed'>('all');
+  const [sort, setSort] = useState<boolean>(true);
 
   const addTodo = (text: string) => {
     const newTodo = { id: Date.now(), text, completed: false };
@@ -43,7 +46,15 @@ const App: React.FC = () => {
     setTodos(todos.filter(todo => !todo.completed));
   };
 
-  const filteredTodos = todos.filter(todo => {
+  const sortedTodos = todos.sort((a, b) => {
+    if (sort) {
+      return a.text.localeCompare(b.text);
+    } else {
+      return b.text.localeCompare(a.text);
+    }
+  });
+
+  const filteredTodos = sortedTodos.filter(todo => {
     if (filter === 'active') return !todo.completed;
     if (filter === 'completed') return todo.completed;
 
@@ -54,21 +65,23 @@ const App: React.FC = () => {
 
   return (
     <Container maxWidth="sm">
-      <AppBar position="static">
+      <AppBar position="static" sx={{ marginBottom: 2 }}>
         <Toolbar>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Theme Button
+            todos
           </Typography>
           <Tooltip title={`Switch to ${isDarkMode ? 'light' : 'dark'} mode`}>
             <IconButton color="inherit" onClick={toggleTheme}>
               {isDarkMode ? <Brightness7Icon /> : <Brightness4Icon />}
             </IconButton>
           </Tooltip>
+          <Tooltip title={`Sort ${sort ? 'Descending' : 'Ascending'}`}>
+            <IconButton color="inherit" onClick={() => setSort(!sort)}>
+              {sort ? <ArrowDownwardIcon /> : <ArrowUpwardIcon />}
+            </IconButton>
+          </Tooltip>
         </Toolbar>
       </AppBar>
-      <Typography variant="h3" gutterBottom sx={{ marginTop: 4 }}>
-        Todo List
-      </Typography>
       <Form addTodo={addTodo} />
       <TodoList todos={filteredTodos} toggleTodo={toggleTodo} removeTodo={removeTodo} />
       <Footer
